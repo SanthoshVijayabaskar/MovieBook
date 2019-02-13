14be6cb1a6967e9cc6e89f5b059c7a47bd07ff84
Santhosh Vijay <santhoshthepro@gmail.com>
2019-02-13 13:07:52 -0500

    Understanding Component Lifecycle Hooks

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnChanges, DoCheck, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked, OnDestroy{
 
-  constructor() { }
+  constructor() {
+    console.log("NavBar: Constructor Executed")
+  }
 
   ngOnInit() {
+    console.log("NavBar: OnInit Executed");
+  }
+
+  ngAfterContentInit() {
+    console.log("NavBar: AfterContentInit Executed");
+  }
+
+  ngAfterContentChecked() {
+    console.log("NavBar: AfterContentChecked Executed");
+  }
+
+  ngAfterViewInit(){
+    console.log("NavBar: AfterViewInit Executed");
+  }
+
+  ngAfterViewChecked(){
+    console.log("NavBar: AfterViewChecked Executed");
+  }
+
+  ngDoCheck(){
+    console.log("NavBar: DoCheck Executed");
+  }
+
+  ngOnChanges(){
+    console.log("NavBar: OnChanges Executed");
+  }
+
+  ngOnDestroy(){
+    console.log("NavBar: OnDestroy Executed");
   }
 
 }
